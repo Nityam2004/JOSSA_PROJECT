6db2821446499e8d292544e83dfed2ad05a3bfcd
nitya <nityamrajput4@gmail.com>
2024-06-13 14:17:36 +0530

    halat kharab

--- FRONTEND/login_page/registeration_system/registeration/registeration/settings.py
+++ FRONTEND/login_page/registeration_system/registeration/registeration/settings.py
@@ -75,5 +75,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
   BASE_DIR,"static"
 ]
+# settings.py
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
